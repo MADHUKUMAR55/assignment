728f4babf929f914e8ccd22e50af1846d4b2168e
madhukumar55 <madhukumar718@gmail.com>
2023-01-21 22:25:46 +0530

    try

---
+++ api/HttpTrigger1/__init__.py
@@ -0,0 +1,23 @@
+
+from sqlalchemy import create_engine
+import json
+import os
+import pandas as pd
+import psycopg2
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        ENV = req.params.get('ENV')
+        conn_str = os.environ[f'CONN_STR_{ENV}'] # Add postgres connection string to env variable CONN_STR_DEV
+        engine = create_engine(conn_str, echo=False)
+        engine.connect()
+        sql = f'''select * from employees;'''
+        result = engine.execute(sql)
+        result_list = [n for n in result]
+        df = pd.DataFrame(result_list, columns=list(result.keys()))
+        records_json = json.loads(df.to_json(orient='records'))
+        return func.HttpResponse(str(records_json))
+    except Exception as e:
+        print(e)
+        return func.HttpResponse(str(e), status_code=500)
